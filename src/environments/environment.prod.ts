67f0e2a30aa3ef36f50a143f715af99943afb256
Srinivas kattamuri <srinivasguptha.k@gmail.com>
2023-09-26 23:43:37 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAO5B-PaXLrksRDowXENZzlzrU-PWpgSOw",
+    authDomain: "srinivas-angular-profile.firebaseapp.com",
+    databaseURL: "https://srinivas-angular-profile.firebaseio.com",
+    projectId: "srinivas-angular-profile",
+    storageBucket: "srinivas-angular-profile.appspot.com",
+    messagingSenderId: "246820008804",
+    appId: "1:246820008804:web:fc64a9d719b82a0b"
+  }
 };
